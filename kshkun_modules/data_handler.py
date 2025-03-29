e4f2671c30544f78098e085c7e9a5f23c77387f8
duzh0 <duzho@proton.me>
2025-03-29 19:12:58 +0100

    refactored sync/async data handler function
    now has a base saving/loading function which is then used synchronously/asynchronously instead of two separate identical functions

--- kshkun_modules/data_handler.py
+++ kshkun_modules/data_handler.py
@@ -1,12 +1,13 @@
-import json
-from .logger import KshkunLogger as klog
+import json, asyncio
+from .logger import KshkunLogger
 
+klog = KshkunLogger()
 
 class SimpleDataHandler:
     def __init__(self):
-        pass
+        klog.log("Initialized SimpleDataHandler")
 
-    def handleDataSync(self, filename: str, data=None):
+    def baseHandleData(self, filename: str, data=None):
         try:
             suffix = 'JSON' if filename.endswith('.json') else 'TXT' if filename.endswith('.txt') else None
             folder = 'json_files/' if suffix == 'JSON' else 'txt_files/' if suffix == 'TXT' else None
@@ -15,7 +16,7 @@ class SimpleDataHandler:
             with open(path, 'w' if data else 'r') as file:
                 if suffix == 'JSON':
                     if save_or_load == 'SAVE':
-                        json.dump(data, file, ensure_ascii=False, indent=4)
+                        json.dump(data, file, ensure_ascii=False, indent=4) # had some problems with saving an empty list, must check later
                     else:    
                         return json.load(file)
                     
@@ -29,31 +30,11 @@ class SimpleDataHandler:
                     raise f'Unsupported file type: .txt or .json expected, got {path}'
                 
         except Exception as e:
-            klog.slog(f'{suffix or "DO_DATA"}_{save_or_load} ERROR: {e}', 'ERROR')
+            klog.err(f'{suffix or "DO_DATA"}_{save_or_load} ERROR: {e}')
             return None
 
+    def handleDataSync(self, filename: str, data=None):
+        return self.baseHandleData(filename, data)
+
     async def handleData(self, filename: str, data=None):
-        try:
-            suffix = 'JSON' if filename.endswith('.json') else 'TXT' if filename.endswith('.txt') else None
-            folder = 'json_files/' if suffix == 'JSON' else 'txt_files/' if suffix == 'TXT' else None
-            path = folder + filename
-            save_or_load = 'LOAD' if data == None else 'SAVE'
-            with open(path, 'w' if data else 'r') as file:
-                if suffix == 'JSON':
-                    if save_or_load == 'SAVE':
-                        json.dump(data, file, ensure_ascii=False, indent=4) # had some problems with saving an empty list, must check later
-                    else:
-                        return json.load(file)
-                    
-                elif suffix == 'TXT':
-                    if save_or_load == 'SAVE':
-                        file.write(data)
-                    else:
-                        return file.read()
-                    
-                else:
-                    raise f'Unsupported file type: .txt or .json expected, got {path}'
-                
-        except Exception as e:
-            await klog.log(f'{suffix or 'DO_DATA'}_{save_or_load} ERROR: {e}', 'ERROR')
-            return None
+        return await asyncio.to_thread(self.baseHandleData, filename, data)
